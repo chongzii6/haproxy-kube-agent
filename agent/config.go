1464c94c3d281a7d95dfda5020fb3540a5b41e59
chenjun <chenjun@molitv.cn>
2018-08-31 17:35:17 +0800

    package agent

--- agent/config.go
+++ agent/config.go
@@ -1,8 +1,10 @@
-package main
+package agent
 
 import (
+	"flag"
 	"io/ioutil"
 	"log"
+	"strings"
 
 	yaml "gopkg.in/yaml.v2"
 )
@@ -26,5 +28,49 @@ func (c *Config) getConf(f string) {
 	err = yaml.Unmarshal(yamlFile, c)
 	if err != nil {
 		log.Fatalf("Unmarshal: %v", err)
+	}
+}
+
+//CmdCfg keeps global configuration
+var CmdCfg = Config{}
+
+//
+func InitConfigFromArgs() {
+	// quiets down kube client library logging
+	configfile := flag.String("config", "", "<optional> path to config yaml file")
+	cafile := flag.String("ca", "", "ca.pem used for etcd")
+	certfile := flag.String("cert", "", "cert.pem used for etcd")
+	keyfile := flag.String("key", "", "key.pem used for etcd")
+	endpoints := flag.String("endpoints", "", "endpoints of etcd")
+	agentkey := flag.String("agent", "", "agent key used in etcd")
+	reqkey := flag.String("req", "", "req key used in etcd")
+	flag.Parse()
+
+	if len(*configfile) > 0 {
+		CmdCfg.getConf(*configfile)
+	}
+
+	if len(*cafile) > 0 {
+		CmdCfg.Cafile = *cafile
+	}
+
+	if len(*certfile) > 0 {
+		CmdCfg.Certfile = *certfile
+	}
+
+	if len(*keyfile) > 0 {
+		CmdCfg.Keyfile = *keyfile
+	}
+
+	if len(*endpoints) > 0 {
+		CmdCfg.Endpoints = strings.Split(*endpoints, ",")
+	}
+
+	if len(*agentkey) > 0 {
+		CmdCfg.Agentkey = *agentkey
+	}
+
+	if len(*reqkey) > 0 {
+		CmdCfg.Reqkey = *reqkey
 	}
 }
--- main.go
+++ main.go
@@ -1,53 +1,9 @@
 package main
 
 import (
-	"flag"
-	"strings"
+	"github.com/chongzii6/haproxy-kube-agent/agent"
 )
 
-//CmdCfg keeps global configuration
-var CmdCfg = Config{}
-
-func initConfig() {
-	// quiets down kube client library logging
-	configfile := flag.String("config", "", "<optional> path to config yaml file")
-	cafile := flag.String("ca", "", "ca.pem used for etcd")
-	certfile := flag.String("cert", "", "cert.pem used for etcd")
-	keyfile := flag.String("key", "", "key.pem used for etcd")
-	endpoints := flag.String("endpoints", "", "endpoints of etcd")
-	agentkey := flag.String("agent", "", "agent key used in etcd")
-	reqkey := flag.String("req", "", "req key used in etcd")
-	flag.Parse()
-
-	if len(*configfile) > 0 {
-		CmdCfg.getConf(*configfile)
-	}
-
-	if len(*cafile) > 0 {
-		CmdCfg.Cafile = *cafile
-	}
-
-	if len(*certfile) > 0 {
-		CmdCfg.Certfile = *certfile
-	}
-
-	if len(*keyfile) > 0 {
-		CmdCfg.Keyfile = *keyfile
-	}
-
-	if len(*endpoints) > 0 {
-		CmdCfg.Endpoints = strings.Split(*endpoints, ",")
-	}
-
-	if len(*agentkey) > 0 {
-		CmdCfg.Agentkey = *agentkey
-	}
-
-	if len(*reqkey) > 0 {
-		CmdCfg.Reqkey = *reqkey
-	}
-}
-
 func main() {
-	initConfig()
+	agent.InitConfigFromArgs()
 }
