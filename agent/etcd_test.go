a8586e19edf42d317576f8391affdfb4e6a1d620
chenjun <chenjun@molitv.cn>
2018-09-04 20:06:37 +0800

    testing

--- agent/etcd_test.go
+++ agent/etcd_test.go
@@ -24,3 +24,46 @@ func TestEtcdWatch(t *testing.T) {
 
 	EtcdWatch(CmdCfg.Reqkey, quit)
 }
+
+func TestWatchAndPut(t *testing.T) {
+	os.Chdir("..")
+	CmdCfg.getConf("agent.yml")
+
+	quit := make(chan int)
+	go func() {
+		<-time.After(time.Second * 2)
+
+		key := CmdCfg.Reqkey + "/testkey"
+		value := `
+		{
+			"action":"add", 
+			"loadbalance_name":"lb1", 
+			"target_port":3800, 
+			"endpoints":[
+				{
+					"name":"server1", 
+					"ip":"192.168.100.210", 
+					"port":38000
+				}, 
+				{
+					"name":"server2", 
+					"ip":"192.168.100.211",
+					"port":38000
+				}
+			]
+		}`
+
+		EtcdPut(key, value)
+		<-time.After(time.Second * 15)
+
+		// reader := bufio.NewReader(os.Stdin)
+		// fmt.Print("Enter text: ")
+		// text, _ := reader.ReadString('\n')
+		// fmt.Println(text)
+
+		quit <- 0
+	}()
+
+	EtcdWatch(CmdCfg.Reqkey, quit)
+
+}
